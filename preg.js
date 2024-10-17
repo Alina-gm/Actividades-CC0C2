bd991836d812dcd0913c944071c7e6f0198d88fd
Alina-gm <Oviedmimim@gmail.com>
2024-10-17 09:28:20 -0500

    Update preg.js

--- preg.js
+++ preg.js
@@ -1,1 +1,19 @@
 /////////////dkjcoidwc
+
+
+import express, { Request, Response } from "express";
+import path from "path";
+
+const server = express();
+const port = 3000;
+
+server.get("/components/weather", function (req: Request, res: Response): void {
+  const filePath = path.join(process.cwd(), "public", "weather.xhtml");
+  res.setHeader("Content-Type", "text/html");
+  res.sendFile(filePath);
+});
+
+server.listen(port, function (): void {
+  console.log("Escuchando en el puerto " + port);
+});
+https://github.com/Alina-gm/Actividades-CC0C2.git
